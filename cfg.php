c959e2d34356d147c7d3f09f9b6d9b0eceeef3f0
agus eko saputra <tembex83@gmail.com>
2021-01-03 18:56:00 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+$useragent = "Mozilla/5.0 (Linux; Android 6.0; Violet_III) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36";
+
+$cookie = "__cfduid=d54c507c640ef690b8ff795ded6b802cc1608742981; __utmz=122368408.1608742991.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not provided); __auc=7071a892176908dfffa1bd25639; PHPSESSID=qoqspg0rk009f27qv0m5soldt7; __asc=ff6ebf83176acc905eab0425df8; __utma=122368408.1650264214.1608742991.1609141444.1609216627.10; __utmc=122368408; _data_html=258-6; __utmt=1; user_id=130137; password=16767ea181abefa45528f7739053b644; cookie_id=0151F8305EC0E95E917868646B9968AC; __utmb=122368408.16.10.1609216627";

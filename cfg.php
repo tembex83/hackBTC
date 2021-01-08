2c2bc3727a9572c04e9b784a1903e98337fd6a2a
agus eko saputra <tembex83@gmail.com>
2021-01-08 12:24:55 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -4,4 +4,4 @@
 
 $useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
 
-$cookie = "__cfduid=d0d15b10282b64bf526791c3e3eb441431609722999; __utmz=122368408.1609827663.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not provided); __auc=6614a6c0176caf7b9acc610734b; PHPSESSID=hgh6d4qirishu00top37l454g2; __asc=a736082e176d908406b3405b36f; __utma=122368408.757939587.1609723001.1609874324.1609958966.5; __utmc=122368408; _data_html=36-4; __utmt=1; user_id=133900; password=23468b9da98e303de9c1a6d37b2f6e65; cookie_id=831C80479A50A297955DE350B6ACF56C; __utmb=122368408.7.10.1609958966";
+$cookie = "__cfduid=d0d15b10282b64bf526791c3e3eb441431609722999; __utmz=122368408.1609827663.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not provided); __auc=6614a6c0176caf7b9acc610734b; PHPSESSID=hgh6d4qirishu00top37l454g2; __asc=769dc194176e06aa15cc6c504f3; __utma=122368408.757939587.1609723001.1610015963.1610082854.8; __utmc=122368408; _data_html=43-4; __utmt=1; user_id=133900; password=23468b9da98e303de9c1a6d37b2f6e65; cookie_id=410B8E93F6A670FE639F7C9C9874EAB9; __utmb=122368408.5.10.1610082854";
